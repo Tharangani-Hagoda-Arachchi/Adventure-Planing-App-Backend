6112ee71a4be772e3980995bf8493fcdb99291f2
Tharangani Hagoda Arachchi <tharangijayamuthu@gmail.com>
2025-09-19 19:23:09 +0530

    uppdation

--- routes/packageRoute.js
+++ routes/packageRoute.js
@@ -1,6 +1,6 @@
 import express from 'express';
 import multer from 'multer';
-import { addpPackage, getPKid } from '../controllers/packageController.js';
+import { addpPackage, getPackageById, getPKid } from '../controllers/packageController.js';
 const upload = multer({ storage: multer.memoryStorage() });
 //import upload from '../upload.js';
 const packagerouter  = express.Router();
@@ -8,6 +8,7 @@ const packagerouter  = express.Router();
 
 packagerouter.post('/packages',upload.single('packageImage'),addpPackage);
 packagerouter.get('/packages/:categoryId',getPKid)
+packagerouter.get('/packages/details/:id',getPackageById)
 
 
 
@@ -201,6 +202,61 @@ export default packagerouter;
  *                   type: string
  *                   example: "Internal server error."
  */
+
+
+//get all site by id
+
+/**
+ * @swagger
+ * /api/packages/details/{id}:
+ *   get:
+ *     summary: get packages details by id
+ *     description: get site details by id.
+ *     tags:
+ *       - Adventure Places
+ *     parameters:
+ *       - in: path
+ *         name: id
+ *         required: true
+ *         schema:
+ *           type: string
+ *         description: ID of place which want to get infoD 
+ * 
+ *     responses:
+ *       200:
+ *         description: site details retrieved successfully.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 message:
+ *                   type: string
+ *                   example: "site details retrieved successfully."
+ *
+ *       500:
+ *         description: Internal server error.
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 success:
+ *                   type: boolean
+ *                   example: false
+ *                 statusCode:
+ *                   type: integer
+ *                   example: 500
+ *                 errorType:
+ *                   type: string
+ *                   example: ServerError
+ *                 message:
+ *                   type: string
+ *                   example: "Internal server error."
+ */
+
+
+
 
 
 
